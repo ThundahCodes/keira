021b6c846074581381d5edb52ea721c2b2dabab6
Crepco <59485770+Crepco@users.noreply.github.com>
2021-01-30 20:30:24 +0300

    Delete tempCodeRunnerFile.py

--- keira/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-@client.command(aliases = ['chatbot'])
-async def cb(ctx, *, phrase):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(f"https://bruhapi.xyz/cb/{phrase}") as response:
-            resp = await response.json()
-            await ctx.send(resp["res"])
-    
